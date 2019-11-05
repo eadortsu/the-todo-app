1edd6dd0d51eb1e4d2d69e797db5ff82a1ee2c43
eadortsu <mail@eadortsu.com>
2019-11-05 11:34:03 +0000

    App Init

---
+++ client/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <p>This is a Vue component and below is the current date:<br />{{date}}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                date: new Date(),
+            };
+        }
+    }
+</script>
+
+<style scoped>
+    p {
+        font-size: 2em;
+        text-align: center;
+    }
+</style>
