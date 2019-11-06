a7574902663d2759e8421ff7fc3147edefdcc9fd
eadortsu <mail@eadortsu.com>
2019-11-06 15:07:28 +0000

    Added Vuetify

--- client/main.js
+++ client/main.js
@@ -1,10 +1,16 @@
 import Vue from 'vue';
 import App from './App.vue';
 import './main.html';
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
+Vue.use(Vuetify)
 
+const vuetify = new Vuetify({})
 Meteor.startup(() => {
   new Vue({
     el: '#app',
+    vuetify,
     ...App,
   });
 });
+
